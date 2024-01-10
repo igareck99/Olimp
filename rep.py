6a2a1141c0d5e8d565a41b7ce8764c7559064562
Igareck99 <igor9.i@yandex.ru>
2024-01-10 18:55:18 +0300

    second commit

--- rep.py
+++ rep.py
@@ -1,1 +1,13 @@
+import csv
+import string
+import secrets
 
+def insertionSort(l: list):
+    for i in range(1, len(l)):
+        temp = l[i]
+        j = i - 1
+        while (j >= 0 and temp[4] < l[j][4]):
+            l[j + 1] = l[j]
+            j = j - 1
+        l[j + 1] = temp
+    return list(reversed(l))
